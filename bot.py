0055d4d00706e030338e09b915c6e56974e5f8ac
SoySoy4444 <sshoinkyuraa.08.04@hotmail.co.jp>
2020-05-22 01:02:20 +0900

    Track scores and determine winner

--- bot.py
+++ bot.py
@@ -13,39 +13,46 @@ GUILD = os.getenv('DISCORD_GUILD')
 
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
-
     for guild in client.guilds:
         if guild.name == GUILD:
             break
 
-    print(
-        f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})'
-    )
-
     members = '\n - '.join([member.name for member in guild.members])
     print(f'Guild Members:\n - {members}')
 
+
 answer_ = "XXXXadsadsasasdsadas"
+usersPlaying = {}
+
+
 @client.event
 async def on_message(message):
-    guild = client.guilds[0]
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
 
     global answer_
+    global usersPlaying
 
-    if "!maths" in message.content:
-        await message.channel.send("Starting new game... enter to quit game.")
+    if message.content == "!maths":
+        usersPlaying = {}
+        await message.channel.send("Starting new game... enter !quit to quit game.")
         question = "4 + 3 + 4"
         await message.channel.send(question)
         answer_ = eval(question)
-        print("ANSWER IS", answer_)
-        print(type(answer_))
 
-    elif message.content.startswith(str(answer_)):
+    elif message.content == str(answer_):
         await message.channel.send(f"Correct! It's {str(answer_)}!")
+        correct_user = message.author  # get the user who answered correctly
+
+        if correct_user in usersPlaying.keys():  # if the answer is already in the database
+            usersPlaying[correct_user] += 1  # increase the user's score
+        else:
+            usersPlaying[correct_user] = 1  # start tracking user score
 
         n1 = str(random.randint(3, 9))
         n2 = str(random.randint(3, 9))
@@ -56,14 +63,17 @@ async def on_message(message):
         question = f"{n1} {o1} {n2} {o2} {n3}"
         await message.channel.send(question)
         answer_ = eval(question)
-        print("ANSWER IS", answer_)
-        print(type(answer_))
 
-    elif "!quit" in message.content:
-        print("TRIGGGG")
+    elif message.content == "!quit":
         await message.channel.send("Thanks for playing!")
-        await message.channel.send("The winner was...")
-        pass
+
+        current_max_user, current_max_score = None, -1
+        for user, score in usersPlaying.items():
+            if score > current_max_score:
+                current_max_user, current_max_score = user, score
+
+        await message.channel.send(f"The winner was {current_max_user} with a score of {current_max_score}!")
+        # TODO: Add leaderboard?
 
 
 client.run(TOKEN)
